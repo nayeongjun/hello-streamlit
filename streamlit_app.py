d5f079c5ea7b5fff0b667aaa36c19440bb0edf71
nayeongjun <162396156+nayeongjun@users.noreply.github.com>
2024-03-27 02:22:39 +0000

    matplotl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import altair as alt
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
-
+rand=np.random.normal(1,2,size=20)
+fig,ax=plt.subplots()
+ax.hist(rand,bins=15)
+st.pyplot(fig)
